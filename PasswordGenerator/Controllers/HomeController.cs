6b9743260a08987db11a5590e7b5008a49600064
Mac PC <rmartinez@igob.com.mx>
2018-12-04 18:54:52 -0600

    Initial Commit

---
+++ PasswordGenerator/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace PasswordGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Random random = new Random();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string GeneratePassword(int Length, bool Numbers, int Letters, bool Special)
+        {
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numbers = "0123456789";
+            var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+            var chars = "";
+
+            if (Numbers)
+                chars += numbers;
+            if (Special)
+                chars += special;
+
+            chars += Letters == 1 ? letters : Letters == 2 ? letters.ToLower() : letters + letters.ToLower();
+
+            return new string(Enumerable.Repeat(chars, Length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
---
+++ PasswordGenerator/Scripts/scripts.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function(){
+
+    var typed = new Typed('#title', {
+        strings: ['','<strong>Simple Password Generator</strong>'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true,
+        smartBackspace: true
+    });
+
+    function createPassword(){
+            var Length = $("#length").val();
+            var Numbers = $("#numbers").is(":checked");
+            var Letters = $("#letters").val();
+            var Special = $("#special").is(":checked");
+
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numbers = "0123456789";
+            var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+            var chars = "";
+            var text ="";
+
+            if (Numbers)
+                chars += numbers;
+            if (Special)
+                chars += special;
+
+            chars += Letters == 1 ? letters : Letters == 2 ? letters.toLowerCase() : letters + letters.toLowerCase();
+
+            for (var i = 0; i < Length; i++)
+                text += chars.charAt(Math.floor(Math.random() * chars.length));
+            
+
+        return text;
+    }
+    
+    $("#form").on("submit",function(e){
+        e.preventDefault();
+
+        axios.post("/Home/GeneratePassword/", {
+            Length: $("#length").val(),
+            Numbers: $("#numbers").is(":checked"),
+            Letters: $("#letters").val(),
+            Special: $("#special").is(":checked")
+        }).then(function(e){
+            $("#result").html("Password: <strong>" + e.data + "</strong>");
+        }).catch(function(e){
+            console.log(e)
+            var result = createPassword();
+            $("#result").html("Password: <strong>" + result + "</strong>");
+        })
+    
+    });
+
+});
